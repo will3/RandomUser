c0e8b946017d49d3bd9825b5f64d51a37ff10891
will <will@rushdigital.co.nz>
2020-05-13 11:40:15 +1200

    Generate template

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by will on 13/05/20.
+//  Copyright © 2020 will. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
